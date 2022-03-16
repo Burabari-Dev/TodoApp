65bc952064b811a76e5e04c7ca6730170e6c94e9
Burabari <burabaridev@gmail.com>
2022-03-16 06:27:59 +0100

    TAC-59 configure Program.cs to use MySQL db

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoDBContext>(
+    opt => opt.UseMySQL("Server=localhost;Port=3306;Database=todo_csharp;User=dev;Password=groovie"));
+builder.Services.AddDbContext<UserDBContext>(
+    opt => opt.UseMySQL("Server=localhost;Port=3306;Database=todo_csharp;User=dev;Password=groovie"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
