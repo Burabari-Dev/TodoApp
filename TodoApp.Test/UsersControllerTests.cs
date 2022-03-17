0526584720f2c449d885807c413896c2e407b07e
Burabari <burabaridev@gmail.com>
2022-03-17 10:32:36 +0100

    TAC-29 create failing test for GetUser method of UsersController

--- TodoApp.Test/UsersControllerTests.cs
+++ TodoApp.Test/UsersControllerTests.cs
@@ -34,6 +34,30 @@ namespace TodoApp.Test
             Assert.True(dbUser.Email == fakeUser.Email);
         }
 
+        [Fact]
+        public void GetUser_Should_Return_The_User_With_The_Specified_Id()
+        {
+            //GIVEN
+            int id = 23;
+            var fakeUser1 = A.Dummy<User>();
+            var fakeUser2 = A.Dummy<User>();
+            var fakeUser3 = A.Dummy<User>();
+            fakeUser2.Id = id;
+            var dataStore = A.Fake<IUserDBContext>();
+            A.CallTo(() => dataStore.GetUser(id)).Returns(Task.FromResult(fakeUser2));
+            var controller = new UsersController(dataStore);
+
+            //WHEN
+            var actionResult = controller.GetUser(id);
+
+            //THEN
+            var result = (OkObjectResult) actionResult.Result;
+            var dbUser = (User)result.Value;
+            Assert.True(dbUser.Id.Equals(fakeUser2.Id));
+            Assert.False(dbUser.Id.Equals(fakeUser1.Id));
+            Assert.False(dbUser.Email.Equals(fakeUser3.Email));
+        }
+
 
     }
 }
