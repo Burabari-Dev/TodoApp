26d5539a05f5e8624c08116380c4c09bafee50cf
Burabari <burabaridev@gmail.com>
2022-03-18 09:40:29 +0100

    TAC-12 create services for getting all users from database

--- TodoApp/Data/IUserDBContext.cs
+++ TodoApp/Data/IUserDBContext.cs
@@ -7,6 +7,7 @@ namespace TodoApp.Data
     {
         public Task<User> AddUser(User user);
         public Task<User> GetUser(int id);
+        public Task<IEnumerable<User>> GetAll();
         public Task<User> UpdateUser(User user);
         public Task<bool> DeleteUser(int id) ;
     }
